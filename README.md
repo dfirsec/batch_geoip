304f770e3a99116db624b214f2a0393af34bfce0
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-11-02 08:17:43 -0400

    Updated

--- README.md
+++ README.md
@@ -1,116 +1,47 @@
 # Batch GeoIP
 
-![Generic badge](https://img.shields.io/badge/python-3.7-blue.svg) [![Twitter](https://img.shields.io/badge/Twitter-@pulsecode-blue.svg)](https://twitter.com/pulsecode)
+![Generic badge](https://img.shields.io/badge/python-3.12-blue.svg) 
 
 Python script to gather IP address geolocation information from different geolocation services.
 
 Results are written to console and file (geo_results.txt).
 
 Sources:
-- *freegeoip.live*
+
 - *tools.keycdn.com*
-- *ipgeolocation.io*
 - *ip-api.com*
 
-# Recommended Installation & Usage
+## Recommended Installation & Usage
 
-```text
-pip install -U pipenv
-git clone https://github.com/dfirsec/batch_geoip.git
-cd batch_geoip
-pipenv install
-pipenv shell
+```console
+└ $ git clone https://github.com/dfirsec/batch_geoip.git
 
-python batch_geoip.py 68.66.224.31
-```
+└ $ cd batch_geoip
 
-# Alternate Installation & Usage
+└ $ poetry install
 
-```text
-$ git clone https://github.com/dfirsec/batch_geoip.git
-$ cd batch_geoip
-$ pip install -r requirements.txt
-
-$ python batch_geoip.py 68.66.224.31
+└ $ poetry shell
 ```
 
 ```console
-Freegeoip Results
---------------------------------------------------
-Ip             : 68.66.224.31
-Country Code   : US
-Country Name   : United States
-Region Code    : MI
-Region Name    : Michigan
-City           : Ann Arbor
-Zip Code       : 48106
-Time Zone      : America/Detroit
-Latitude       : 42.2503
-Longitude      : -83.8393
-Metro Code     : 505
+└ $ python batch_geoip.py 68.66.224.31
 
-KeyCDN Results
+Combined Results
 --------------------------------------------------
-Host           : 68.66.224.31
 Ip             : 68.66.224.31
+As             : AS55293 A2 Hosting, Inc.
+Isp            : A2 Hosting, Inc.
 Rdns           : az1-ls8.a2hosting.com
-Asn            : 55293
-Isp            : A2HOSTING
-Country Name   : United States
-Country Code   : US
-Continent Name : North America
-Continent Code : NA
-Latitude       : 37.751
-Longitude      : -97.822
-Timezone       : America/Chicago
-Datetime       : 2020-09-01 06:16:53
-
-IP Geolocation Results
---------------------------------------------------
-IP                                 : 68.66.224.31
-Continent Code                     : NA
-Continent Name                     : North America
-Country Code (ISO 3166-1 alpha-2)  : US
-Country Code (ISO 3166-1 alpha-3)  : USA
-Country Name                       : United States
-Country Capital                    : Washington
-State/Province                     : Michigan
-District/County                    : Bach
-City                               : Ann Arbor
-Zip Code                           : 48104
-Latitude & Longitude of City       : 42.28080 , -83.74300
-Geoname ID                         : 4984247
-Is EU?                             : false
-Calling Code                       : +1
-Country TLD                        : .us
-Languages                          : en-US,es-US,haw,fr
-ISP                                : A2 Hosting
-Organization                       : A2 Hosting, Inc.
-AS Number                          : AS55293
-Currency                           : US Dollar
-Currency Code                      : USD
-Currency Symbol                    : $
-Timezone                           : America/Detroit
-Timezone Offset                    : -5
-Current Time                       : 2020-09-01 07:16:53.501-0400
-Current Time Unix                  : 1.598959013501E9
-Is DST?                            : true
-DST Savings                        : 1
-
-IP-API Results
---------------------------------------------------
-Status         : success
 Country        : United States
 Countrycode    : US
-Region         : MI
-Regionname     : Michigan
-City           : Ann Arbor
-Zip            : 48106
-Lat            : 42.2583
-Lon            : -83.6814
-Timezone       : America/Detroit
-Isp            : A2 Hosting, Inc.
-Org            : A2 Hosting, Inc
-As             : AS55293 A2 Hosting, Inc.
-Query          : 68.66.224.31
+Regionname     : Arizona
+Region         : AZ
+City           : Phoenix
+Zip            : 85001
+Continent Name : North America
+Continent Code : NA
+Lat            : 33.4484
+Lon            : -112.074
+Timezone       : America/Phoenix
+Datetime       : 2023-11-02 07:14:36
 ```
--- batch_geoip.py
+++ batch_geoip.py
@@ -11,7 +11,7 @@ import requests
 from colorama import Fore
 from colorama import init
 
-# Console Colors
+# console colors and formatting
 init()
 CYAN = Fore.CYAN
 RESET = Fore.RESET
@@ -19,11 +19,7 @@ RESET = Fore.RESET
 
 @contextmanager
 def write_to_file(file_path: Path) -> Generator[Callable[[str], int], None, None]:
-    """Context manager for writing to file.
-
-    Yields:
-        [type]: [description]
-    """
+    """Context manager for writing to file."""
     with file_path.open("a", encoding="utf-8") as file:
         yield file.write
 
@@ -48,49 +44,106 @@ class BatchWorker:
         return requests.get(url, headers=headers, timeout=5)
 
     def process(self: "BatchWorker") -> None:
-        """Takes a URL, connects to it, and then writes the results to a file."""
+        """Takes URLs, connects to them, and then writes the combined results to a file."""
+        combined_results = {}
+
+        # map similar keys from both sources to a single key
+        key_mappings = {
+            "country_name": "country",
+            "country_code": "countrycode",
+            "region_name": "regionname",
+            "region_code": "region",
+            "postal_code": "zip",
+            "latitude": "lat",
+            "longitude": "lon",
+            "asn": "as",
+            "isp": "isp",  # Separate 'isp' and 'org'
+            "org": "org",
+            "host": "ip",
+            "query": "ip",
+        }
+
+        def add_to_combined_results(data: dict) -> None:
+            for k, v in data.items():
+                key = key_mappings.get(k.lower(), k.lower())
+                if key not in combined_results or combined_results[key] != v:
+                    combined_results[key] = v
+
         # tools.keycdn.com
         try:
-            self.keycdn()
+            kc_data = self.keycdn()
+            add_to_combined_results(kc_data)
         except Exception as err:
             print(err)
 
         # ip-api
         try:
-            self.ipapi()
+            ipapi_data = self.ipapi()
+            add_to_combined_results(ipapi_data)
         except Exception as err:
             print(err)
 
-    def keycdn(self: "BatchWorker") -> None:
-        """Connect to tools.keycdn.com and write the results to a file."""
+        # combine and Print Results
+        self.print_and_save_results(combined_results)
+
+    def keycdn(self: "BatchWorker") -> dict:
+        """Connect to tools.keycdn.com and return the results."""
         headers = {"User-Agent": f"keycdn-tools:https://{self.host}"}
         kc_url = requests.get(self.kc_url, headers=headers, timeout=5).json()
-        results = [f"\nKeyCDN Results\n{('-' * 50)}\n"]
-        print(f"\n{CYAN}KeyCDN Results{RESET}\n{('-' * 50)}")
+        return kc_url["data"]["geo"]
 
-        for _key, _value in kc_url["data"]["geo"].items():
-            if _value:
-                formatted_line = f"{_key.title().replace('_', ' '):15}: {_value}\n"
-                results.append(formatted_line)
+    def ipapi(self: "BatchWorker") -> dict:
+        """Connect to ip-api.com and return the results."""
+        return self.connect(self.ipapi_url).json()
+
+    def print_and_save_results(self: "BatchWorker", results: dict) -> None:
+        """Print and save the combined results."""
+        # if 'Isp' and 'Org' are the same, remove 'Org'
+        if results["org"] == results["isp"]:
+            results.pop("org", None)
+
+        # define the order of the keys to be printed
+        ordered_keys = [
+            "ip",
+            "org",
+            "as",
+            "isp",
+            "rdns",
+            "country",
+            "countrycode",
+            "regionname",
+            "region",
+            "city",
+            "zip",
+            "metro_code",
+            "continent_name",
+            "continent_code",
+            "lat",
+            "lon",
+            "timezone",
+            "datetime",
+            # "status",
+        ]
+
+        # correcting keys to match the display format
+        display_keys = {
+            "metro_code": "Metro Code",
+            "continent_name": "Continent Name",
+            "continent_code": "Continent Code",
+        }
+
+        formatted_results = [f"\nCombined Results\n{('-' * 50)}\n"]
+        print(f"\n{CYAN}Combined Results{RESET}\n{('-' * 50)}")
+
+        for key in ordered_keys:
+            display_key = display_keys.get(key, key.title().replace("_", " "))
+            if key in results and results[key]:
+                formatted_line = f"{display_key:15}: {results[key]}\n"
+                formatted_results.append(formatted_line)
                 print(formatted_line.strip())
 
         with write_to_file(self.geo_results) as writer:
-            writer("".join(results))
-
-    def ipapi(self: "BatchWorker") -> None:
-        """Connect to ip-api.com and write the results to a file."""
-        ipapi_url = self.connect(self.ipapi_url).json()
-        results = [f"\nIP-API Results\n{('-' * 50)}\n"]
-        print(f"\n{CYAN}IP-API Results{RESET}\n{('-' * 50)}")
-
-        for _key, _value in ipapi_url.items():
-            if _value:
-                formatted_line = f"{_key.title():15}: {_value}\n"
-                results.append(formatted_line)
-                print(formatted_line.strip())
-
-        with write_to_file(self.geo_results) as writer:
-            writer("".join(results))
+            writer("".join(formatted_results))
 
 
 def main() -> None:
